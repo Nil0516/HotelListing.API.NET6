173b90da683dee3d8cecb02ed106e03e9cb6e6ae
Nil <nil_huang@cmoney.com.tw>
2023-02-26 00:43:59 +0800

    feat: Added Hotels API Endpoing

---
+++ HotelListing.API/Contracts/IHotelsRepository.cs
@@ -0,0 +1,9 @@
+﻿using HotelListing.API.Data;
+
+namespace HotelListing.API.Contracts
+{
+    public interface IHotelsRepository : IGenericRepository<Hotel>
+    {
+        Task<Hotel> GetHotelDetail(int id);
+    }
+}
---
+++ HotelListing.API/Repository/HotelsRepository.cs
@@ -0,0 +1,22 @@
+﻿using HotelListing.API.Contracts;
+using HotelListing.API.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.API.Repository
+{
+    public class HotelsRepository : GenericRepository<Hotel>, IHotelsRepository
+    {
+        private readonly HotelListListingDbContext _context;
+
+        public HotelsRepository(HotelListListingDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<Hotel> GetHotelDetail(int id)
+        {
+            return await _context.Hotels.Include(q => q.Country)
+                                        .FirstOrDefaultAsync(q => q.Id == id);
+        }
+    }
+}
